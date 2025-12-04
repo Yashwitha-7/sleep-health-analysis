57996704645109d33dc37993eeeca11585a97407
Yashwitha-7 <97009149+Yashwitha-7@users.noreply.github.com>
2025-12-04 01:55:38 -0500

    Remove font color setting in plotly chart

--- app.py
+++ app.py
@@ -2072,7 +2072,6 @@ elif page == " Advanced Techniques":
             plot_bgcolor='#F9F6E4',
             paper_bgcolor='#F9F6E4',
             title_font_color='#F28B75',
-            font=dict(color='#2C3E50')
         )
         
         st.plotly_chart(fig, use_container_width=True)
